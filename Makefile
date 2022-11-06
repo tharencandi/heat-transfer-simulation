80e93cb603fe3d659a926a8acce3ca670d7451f5
Tharen Candi <candi.tharen@gmail.com>
2022-11-06 12:00:21 +0100

    block fix:

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ CC = gcc
 CFLAGS = -std=gnu99 -O3 -fopenmp -Wall
 
 all: heatmap sharing
+debug_heatmap: heatmap.c utility.h algorithm.c
+	$(CC) $(CFLAGS) -g $< -o $@
 
 heatmap: heatmap.c utility.h algorithm.c
 	$(CC) $(CFLAGS) $< -o $@
@@ -11,4 +13,4 @@ sharing: sharing.c utility.h
 	$(CC) $(CFLAGS) $< -o $@
 
 clean:
-	rm -f heatmap sharing
+	rm -f heatmap sharing debug_heatmap
--- algorithm.c
+++ algorithm.c
@@ -6,42 +6,67 @@ SCIPER		: 359559, 359399
 ============================================================================
 */
 #include <math.h>
-
+// elements with same j but different i are far apart.
+//elements with same i but different j are close.
 #define INPUT(I,J) input[(I)*length+(J)]
 #define OUTPUT(I,J) output[(I)*length+(J)]
-#define BLOCK_SIZE 4
+#define BLOCK_SIZE 64
 void simulate(double *input, double *output, int threads, int length, int iterations)
 {
+
+    /*
+        data is brought into cache BY THE BLOCK. therefore, its optimal to access neigbouring elements
+        within a block to prevent cache misses. 
+
+
+        finding: llc misses are coming from the n iterations::
+
+        is it possible to perform n iterations on the cacheblock when the cache block is loaded. 
+
+        can we also incorporate unrolling?
+
+    */
     double *temp;
-     omp_set_num_threads(threads);
+    omp_set_num_threads(threads);
     // Parallelize this!!
     for(int n=0; n < iterations; n++)
     {   
-        #pragma omp parallel for shared(output)
-        for(int jj = 0; jj < length -3; jj += BLOCK_SIZE/2) {
-            for(int kk = 0; kk < length- 3; kk += BLOCK_SIZE/2) {
-                
+        //#pragma omp parallel for shared(output)
+        for(int jj = 0; jj <= length - BLOCK_SIZE+2; jj += BLOCK_SIZE-2) {
+            for(int kk = 0; kk <= length- BLOCK_SIZE+2; kk += BLOCK_SIZE-2) {
+               
                 for (int i = jj+1; i < BLOCK_SIZE + jj -1 ; i ++ ) {
                     for (int j = kk+1; j < BLOCK_SIZE + kk -1; j++) {
-
+                      
                         if ( ((i == length/2-1) || (i== length/2))
                         && ((j == length/2-1) || (j == length/2)) )
                         continue;
+
                         OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
                                     INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
                                     INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
                     }
                 }
-               
-
-
-
+                // printf("count %d\n", count * sizeof(int)); = 4 elems / 16bytes 
+                
             }
         }
         
 
+        temp = input;
+        input = output;
+        output = temp;
+    
+    }
+}
 
-    /*
+
+void simulate_base(double *input, double *output, int threads, int length, int iterations) {
+    double *temp;
+    
+    // Parallelize this!!
+    for(int n=0; n < iterations; n++)
+    {
         for(int i=1; i<length-1; i++)
         {
             for(int j=1; j<length-1; j++)
@@ -55,10 +80,9 @@ void simulate(double *input, double *output, int threads, int length, int iterat
                                    INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
             }
         }
-    */
+
         temp = input;
         input = output;
         output = temp;
-    
     }
 }
