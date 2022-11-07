c5ba71c9633ed1191fe99512c4b48871fd252b15
Tharen Candi <candi.tharen@gmail.com>
2022-11-07 18:24:43 +0100

    original :

--- algorithm.c
+++ algorithm.c
@@ -186,3 +186,31 @@ void simulate_unroll(double *input, double *output, int threads, int length, int
     }
 }
 
+
+void simulate_original(double *input, double *output, int threads, int length, int iterations)
+{
+    double *temp;
+    
+    // Parallelize this!!
+    for(int n=0; n < iterations; n++)
+    {
+        for(int i=1; i<length-1; i++)
+        {
+            for(int j=1; j<length-1; j++)
+            {
+                    if ( ((i == length/2-1) || (i== length/2))
+                        && ((j == length/2-1) || (j == length/2)) )
+                        continue;
+
+                    OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
+                                   INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
+                                   INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
+            }
+        }
+
+        temp = input;
+        input = output;
+        output = temp;
+    }
+}
+
