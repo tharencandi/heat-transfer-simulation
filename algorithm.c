4db7ea17f2d6a1dd47ae0af5334d465b99e31f5d
Herman Thong <hermanthong@gmail.com>
2022-11-07 16:09:00 +0100

    add unroll

--- algorithm.c
+++ algorithm.c
@@ -176,15 +176,65 @@ void simulate_base(double *input, double *output, int threads, int length, int i
     omp_set_num_threads(threads);
    
   
-    #pragma omp parallel 
+    // #pragma omp parallel 
     {
     for(int n=0; n < iterations; n++)
     {   
-        #pragma omp for schedule(static) 
+        #pragma omp parallel for schedule(static)
         //#pragma omp tile sizes(BLOCK_SIZE,BLOCK_SIZE, 6)
         for(int i=1; i<length-1; i++)
         {     
-            for(int j=1; j<length-1; j++)
+            for(int j=1; j<length-1; j += 2)
+            {
+                    if ( ((i == length/2-1) || (i== length/2))
+                        && ((j == length/2-1) || (j == length/2)) )
+                        continue;
+
+                    OUTPUT_O(i,j) = (INPUT_O(i-1,j-1) + INPUT_O(i-1,j) + INPUT_O(i-1,j+1) +
+                                   INPUT_O(i,j-1)   + INPUT_O(i,j)   + INPUT_O(i,j+1)   +
+                                   INPUT_O(i+1,j-1) + INPUT_O(i+1,j) + INPUT_O(i+1,j+1) )/9;
+                    
+                    if ((j+1 == length/2-1) || (j+1 == length/2))
+                        continue;
+
+                    OUTPUT_O(i,j+1) = (INPUT_O(i-1,j) + INPUT_O(i-1,j+1) + INPUT_O(i-1,j+2) +
+                                   INPUT_O(i,j)   + INPUT_O(i,j+1)   + INPUT_O(i,j+2)   +
+                                   INPUT_O(i+1,j) + INPUT_O(i+1,j+1) + INPUT_O(i+1,j+2) )/9;
+
+                    if ((j+1 == length/2-1) || (j+1 == length/2))
+                        continue;
+
+                    OUTPUT_O(i,j+1) = (INPUT_O(i-1,j) + INPUT_O(i-1,j+1) + INPUT_O(i-1,j+2) +
+                                   INPUT_O(i,j)   + INPUT_O(i,j+1)   + INPUT_O(i,j+2)   +
+                                   INPUT_O(i+1,j) + INPUT_O(i+1,j+1) + INPUT_O(i+1,j+2) )/9;
+            }
+        }
+
+        #pragma omp single
+        {
+            temp = input;
+            input = output;
+            output = temp;
+
+        }
+    }
+    }
+}
+
+void simulate_base(double *input, double *output, int threads, int length, int iterations) {
+    double *temp;
+    omp_set_num_threads(threads);
+   
+  
+    // #pragma omp parallel 
+    {
+    for(int n=0; n < iterations; n++)
+    {   
+        #pragma omp parallel for schedule(static)
+        //#pragma omp tile sizes(BLOCK_SIZE,BLOCK_SIZE, 6)
+        for(int i=1; i<length-1; i++)
+        {     
+            for(int j=1; j<length-1; j ++)
             {
                     if ( ((i == length/2-1) || (i== length/2))
                         && ((j == length/2-1) || (j == length/2)) )
@@ -206,3 +256,47 @@ void simulate_base(double *input, double *output, int threads, int length, int i
     }
     }
 }
+
+void simulate_unroll(double *input, double *output, int threads, int length, int iterations) {
+    double *temp;
+    omp_set_num_threads(threads);
+   
+  
+    // #pragma omp parallel 
+    {
+    for(int n=0; n < iterations; n++)
+    {   
+        #pragma omp parallel for schedule(static)
+        //#pragma omp tile sizes(BLOCK_SIZE,BLOCK_SIZE, 6)
+        for(int i=1; i<length-1; i++)
+        {     
+            for(int j=1; j<length-1; j += 2)
+            {
+                    if ( ((i == length/2-1) || (i== length/2))
+                        && ((j == length/2-1) || (j == length/2)) )
+                        continue;
+
+                    OUTPUT_O(i,j) = (INPUT_O(i-1,j-1) + INPUT_O(i-1,j) + INPUT_O(i-1,j+1) +
+                                   INPUT_O(i,j-1)   + INPUT_O(i,j)   + INPUT_O(i,j+1)   +
+                                   INPUT_O(i+1,j-1) + INPUT_O(i+1,j) + INPUT_O(i+1,j+1) )/9;
+                    
+                    if ((j+1 == length/2-1) || (j+1 == length/2))
+                        continue;
+
+                    OUTPUT_O(i,j+1) = (INPUT_O(i-1,j) + INPUT_O(i-1,j+1) + INPUT_O(i-1,j+2) +
+                                   INPUT_O(i,j)   + INPUT_O(i,j+1)   + INPUT_O(i,j+2)   +
+                                   INPUT_O(i+1,j) + INPUT_O(i+1,j+1) + INPUT_O(i+1,j+2) )/9;
+            }
+        }
+
+        #pragma omp single
+        {
+            temp = input;
+            input = output;
+            output = temp;
+
+        }
+    }
+    }
+}
+
